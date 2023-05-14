7a3eb9472f65dc79360481d31492553507174166
JamesBremner <james@ravenspoint.com>
2023-05-14 13:09:10 -0400

    Specify problem in text file

--- src/cJobStage.cpp
+++ src/cJobStage.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <iostream>
+#include <fstream>
 
 /*
 Problem:
@@ -27,14 +28,13 @@ public:
     {
         return myfDone;
     }
-    void run( int& t)
+    void run(int &t)
     {
         std::cout << "Machine " << myMachine
-            << " starts at " << t;
-                  t += myDuration;
+                  << " starts at " << t;
+        t += myDuration;
         myfDone = true;
         std::cout << " completes at " << t << "\n";
-
     }
 
 private:
@@ -45,6 +45,10 @@ private:
 class cJobStages
 {
 public:
+    void clear()
+    {
+        myStage.clear();
+    }
     void add(const cStage &stage)
     {
         myStage.push_back(stage);
@@ -64,15 +68,85 @@ private:
     std::vector<cStage> myStage;
 };
 
+class cSchedule
+{
+public:
+    void clear()
+    {
+        myJobs.clear();
+    }
+    void add(const cJobStages &job)
+    {
+        myJobs.push_back(job);
+    }
+    int jobCount() const
+    {
+        return myJobs.size();
+    }
+    void run();
+
+private:
+    std::vector<cJobStages> myJobs;
+};
+
+void cSchedule::run()
+{
+    int t = 0;
+    for (auto &j : myJobs)
+        j.nextStage().run(t);
+}
+/// @brief read problem specification from file
+/// @param sked schedule to be populated
+/// @param fname path to specification file
+/// Fole format documentation https://github.com/JamesBremner/schedule/wiki/Stage-Jobs
+
+void readfile(cSchedule &sked, const std::string &fname)
+{
+    sked.clear();
+    std::ifstream ifs(fname);
+    if (!ifs.is_open())
+        throw std::runtime_error(
+            "Cannot open input file");
+    std::string linetype;
+    int machine, duration;
+    cJobStages job;
+
+    bool firstjob = true;
+    while (ifs.good())
+    {
+        ifs >> linetype;
+        switch (linetype[0])
+        {
+        case 'j':
+            if( firstjob ) {
+                firstjob = false;
+                break;
+            }
+            sked.add(job);
+            job.clear();
+            break;
+        case 's':
+            ifs >> machine >> duration;
+            cStage stage(machine, duration);
+            job.add(stage);
+            break;
+        }
+    }
+    sked.add(job);
+}
+
 int main()
 {
+    cSchedule sked;
     cJobStages job1;
     cJobStages job2;
     job1.add(cStage(1, 2));
     job2.add(cStage(1, 3));
+    sked.add(job1);
+    sked.add(job2);
 
-    int t = 0;
-    job1.nextStage().run( t );
-    job2.nextStage().run(t);
+    sked.run();
 
+    readfile( sked, "../problems/stage.txt");
+    sked.run();
 }
